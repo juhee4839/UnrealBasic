d6e93817dfb0b42b2ca3e8b19ca131752e077e05
kim ju hee <79627080+juhee4839@users.noreply.github.com>
2024-12-26 11:22:02 +0900

    Apply .gitignore and clean up repository

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/HelloUnral/UHT/HelloUnralGameModeBase.generated.h
@@ -0,0 +1,98 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "HelloUnralGameModeBase.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef HELLOUNRAL_HelloUnralGameModeBase_generated_h
+#error "HelloUnralGameModeBase.generated.h already included, missing '#pragma once' in HelloUnralGameModeBase.h"
+#endif
+#define HELLOUNRAL_HelloUnralGameModeBase_generated_h
+
+#define FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_SPARSE_DATA
+#define FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_RPC_WRAPPERS
+#define FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_ACCESSORS
+#define FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAHelloUnralGameModeBase(); \
+	friend struct Z_Construct_UClass_AHelloUnralGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AHelloUnralGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/HelloUnral"), NO_API) \
+	DECLARE_SERIALIZER(AHelloUnralGameModeBase)
+
+
+#define FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAHelloUnralGameModeBase(); \
+	friend struct Z_Construct_UClass_AHelloUnralGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AHelloUnralGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/HelloUnral"), NO_API) \
+	DECLARE_SERIALIZER(AHelloUnralGameModeBase)
+
+
+#define FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AHelloUnralGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AHelloUnralGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AHelloUnralGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AHelloUnralGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AHelloUnralGameModeBase(AHelloUnralGameModeBase&&); \
+	NO_API AHelloUnralGameModeBase(const AHelloUnralGameModeBase&); \
+public: \
+	NO_API virtual ~AHelloUnralGameModeBase();
+
+
+#define FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AHelloUnralGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AHelloUnralGameModeBase(AHelloUnralGameModeBase&&); \
+	NO_API AHelloUnralGameModeBase(const AHelloUnralGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AHelloUnralGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AHelloUnralGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AHelloUnralGameModeBase) \
+	NO_API virtual ~AHelloUnralGameModeBase();
+
+
+#define FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_12_PROLOG
+#define FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_SPARSE_DATA \
+	FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_RPC_WRAPPERS \
+	FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_ACCESSORS \
+	FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_INCLASS \
+	FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_SPARSE_DATA \
+	FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_ACCESSORS \
+	FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> HELLOUNRAL_API UClass* StaticClass<class AHelloUnralGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_JH_UnrealPortfolio_UnrealBasic_Source_HelloUnral_HelloUnralGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
