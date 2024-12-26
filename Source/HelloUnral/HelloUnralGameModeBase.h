7e099f18135a6fbcb80fc4bfcb159c528286e760
kim ju hee <79627080+juhee4839@users.noreply.github.com>
2024-12-26 10:47:05 +0900

    Add project files.

---
+++ Source/HelloUnral/HelloUnralGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HelloUnralGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLOUNRAL_API AHelloUnralGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
